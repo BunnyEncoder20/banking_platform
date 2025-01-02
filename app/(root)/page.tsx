9b1f0b0e5d2712d6c4455453e7d2a7e1ade6ec6e
Bunny's Macbook M1 <varun.verma2024@gmail.com>
2025-01-02 19:18:09 +0530

    implements headerBox

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,27 @@
 import React from "react";
 
+// components
+import HeaderBox from "@/components/HeaderBox";
+
+// current page 📄
 const HomePage = () => {
-  return <div>Horizon Home Page</div>;
+  const loggedIn = {
+    firstname: "Bunny",
+  };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome, "
+            user={loggedIn?.firstname ?? "Guest"}
+            subtext="Access and manage your account and transactions with ease."
+          />
+        </header>
+      </div>
+    </section>
+  );
 };
 
 export default HomePage;
