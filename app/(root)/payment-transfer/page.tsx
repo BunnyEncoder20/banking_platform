0c34ec00115927387134858302d1783c86bf73a3
Bunny's Macbook M1 <varun.verma2024@gmail.com>
2025-01-10 21:34:51 +0530

    Implementes BankDropdown Comp

--- app/(root)/payment-transfer/page.tsx
+++ app/(root)/payment-transfer/page.tsx
@@ -1,7 +1,21 @@
 import React from "react";
 
+// components
+import HeaderBox from "@/components/HeaderBox";
+
+// current page 📄
 const paymentTransfer = () => {
-  return <div>paymentTransfer</div>;
+  return (
+    <section className="payment-transfer">
+      <HeaderBox
+        title="Payment Transfer"
+        subtext="Please fill out the form below to initiate a payment transfer"
+      />
+
+      <section className="size-full pt-5">
+      </section>
+    </section>
+  );
 };
 
 export default paymentTransfer;
